f6c9a6d94b289b1eab20b87d2d1a720119541ff2
Dogyun Jeong <dogyunjeong@gmail.com>
2020-06-05 13:47:06 +0900

    feat: implement producing progress time

--- web/src/Item.ts
+++ web/src/Item.ts
@@ -9,6 +9,7 @@ class Item {
   private _wrapper: HTMLElement
   private _upgradeCostElem: HTMLElement
   private _revenueElem: HTMLElement
+  private _progressElem: HTMLElement
 
   private _revenue: number
   private _upgradeCost: number
@@ -27,64 +28,74 @@ class Item {
     this._parent = parent
 
     // Initializing component
-    const revenue: number = this._itemConfig.revenueFn(this._itemConfig.level)
-    const upgradeCost: number = this._itemConfig.upgradeCostFn(this._itemConfig.level)
-    const wrapper = document.createElement('div')
-    wrapper.className = 'item'
-    wrapper.id = this._itemConfig.name
+    this._wrapper = document.createElement('div')
+    this._wrapper.className = 'item'
+    this._wrapper.id = this._itemConfig.name
 
     const titleElem = document.createElement('div')
     titleElem.className = 'name'
     titleElem.innerText = this._itemConfig.name
-    titleElem.onclick = this._produce
-
-    const revenueElem = document.createElement('div')
-    revenueElem.className = 'revenue'
-    revenueElem.innerText = `$ ${revenue.toString()}`
+    titleElem.onclick = this.handleProduce
 
     const productionTime = document.createElement('div')
     productionTime.className = 'production-time'
     productionTime.innerText = `take: ${this._itemConfig.productionTime} s`
 
-    const upgradeCostElem = document.createElement('div')
-    upgradeCostElem.className = 'upgrade'
-    upgradeCostElem.onclick = this._upgrade
-    upgradeCostElem.innerText = `upgrade: ${upgradeCost}`
+    this._revenueElem = document.createElement('div')
+    this._revenueElem.className = 'revenue'
 
-    this._wrapper = wrapper
-    this._upgradeCostElem = upgradeCostElem
-    this._revenueElem = revenueElem
-    wrapper.appendChild(titleElem)
-    wrapper.appendChild(revenueElem)
-    wrapper.appendChild(productionTime)
-    wrapper.appendChild(upgradeCostElem)
-    this._parent.appendChild(wrapper)
+    this._upgradeCostElem = document.createElement('div')
+    this._upgradeCostElem.className = 'upgrade'
+    this._upgradeCostElem.onclick = this.handleUpgrade
+
+    this._progressElem = document.createElement('div')
+    this._progressElem .className = 'progress'
+
+    this._wrapper.appendChild(titleElem)
+    this._wrapper.appendChild(this._revenueElem)
+    this._wrapper.appendChild(productionTime)
+    this._wrapper.appendChild(this._upgradeCostElem)
+    this._wrapper.appendChild(this._progressElem)
+    this._parent.appendChild(this._wrapper)
+
+    // Render update cost and revenue
+    this.renderRevenueAndUpgradeCost()
   }
 
-  private _upgrade = () => {
+  private handleUpgrade = () => {
     const level = this._itemConfig.level + 1
     this._itemConfig = {
       ...this._itemConfig,
       level,
     }
-    this._updateRender()
+    this.renderRevenueAndUpgradeCost()
+    this._revenue = this._itemConfig.revenueFn(this._itemConfig.level)
+    this._upgradeCost = this._itemConfig.upgradeCostFn(this._itemConfig.level)
   }
 
-  private _produce = () => {
+  private handleProduce = () => {
     if (this._isProducing) {
       return
     }
+    this._isProducing = true
     this._productionStartAt = new Date()
+    let progressTime = 0
+    this._progressElem.innerText = `${(progressTime / 1000).toFixed(1)} s`
+    const interval = setInterval(() => {
+      progressTime += 100
+      this._progressElem.innerText = `${(progressTime / 1000).toFixed(1)} s`
+    }, 100)
     setTimeout(() => {
+      clearInterval(interval)
+      this._progressElem.innerText = ''
       this._onAddMoney(this._revenue)
+      this._isProducing = false
     }, this._itemConfig.productionTime * 1000)
   }
 
-  private _updateRender () {
-    this._revenue = this._itemConfig.revenueFn(this._itemConfig.level)
-    this._upgradeCost = this._itemConfig.upgradeCostFn(this._itemConfig.level)
-    this._revenueElem.innerText = this._revenue.toString()
-    this._upgradeCostElem.innerText = this._upgradeCost.toString()
+  private renderRevenueAndUpgradeCost () {
+    this._upgradeCostElem.innerText =  `upgrade: ${this._upgradeCost.toString()}`
+    this._revenueElem.innerText = `$ ${this._revenue.toString()}`
   }
 }
 
